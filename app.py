370b8c7bc439d15389acb84b41a3e9494ce2531d
tantomusika-stack <tantomusika@gmail.com>
2026-09-11 17:29:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -316,7 +316,8 @@ else:
                     
                     if dl_id:
                         c = conn.cursor()
-                        c.execute("SELECT file_name, file_data FROM reports WHERE id=?", (dl_id,))
+                        # PERBAIKAN: Bungkus dl_id dengan int() agar terbaca oleh SQLite
+                        c.execute("SELECT file_name, file_data FROM reports WHERE id=?", (int(dl_id),))
                         row = c.fetchone()
                         
                         if row:
@@ -337,7 +338,8 @@ else:
                     
                     if st.button("Hapus File Ini", use_container_width=True):
                         c = conn.cursor()
-                        c.execute("DELETE FROM reports WHERE id=?", (del_id,))
+                        # PERBAIKAN: Bungkus del_id dengan int() agar terbaca oleh SQLite
+                        c.execute("DELETE FROM reports WHERE id=?", (int(del_id),))
                         conn.commit()
                         st.success(f"✅ Laporan dengan ID {del_id} berhasil dihapus dari sistem!")
                         st.rerun()
